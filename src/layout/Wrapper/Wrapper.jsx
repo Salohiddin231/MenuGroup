00dca5e9c6753952d80a1271e0e76f7ea51b2cab
Salohiddin231 <davletovsalohiddin231@gmail.com>
2025-06-22 19:21:07 +0300

    update_sidebar

--- src/layout/Wrapper/Wrapper.jsx
+++ src/layout/Wrapper/Wrapper.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/SiteBar";
 import { RoutePage } from "./components/RoutePage";
 
 export default function Wrapper() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-    <div className="home">
-      <Sidebar />
-      <RoutePage />
+    <div className={`home ${isSidebarOpen ? 'sidebar-open' : ''}`}>
+      <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+      <div className="routes">
+        <RoutePage />
+      </div>
     </div>
   );
 }
