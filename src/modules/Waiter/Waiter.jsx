ca30180f3474af08fc67d14f0d93454d34f650d0
muhammadali200702 <githubali007@gmail.com>
2025-06-23 12:43:26 +0500

    witer

--- src/modules/Waiter/Waiter.jsx
+++ src/modules/Waiter/Waiter.jsx
@@ -1,13 +1,72 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
+import { useEffect, useState } from "react";
+import { MdEditDocument } from "react-icons/md";
+import { RiDeleteBinFill } from "react-icons/ri";
+import Header from "../../components/Header/Header";
+import Waiter_modal from "./Waiter_item/Waiter_modal"; // Убедись, что путь правильный
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "../../assets/scss/modules/_wait.scss";
 
 export default function Waiter() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
+  useEffect(() => {
+    AOS.init({ once: true, duration: 800 });
+  }, []);
+
   return (
     <>
-    <Header text={'Официанты'}/>
-    <section className="wait">
-      {/* Пишите с новой ветки */}
-    </section>
+      <Header text="Официанты" />
+
+      <section className="wait">
+        <button onClick={() => setModalOpen(true)} className="wait_add">
+          + Добавить Официанта
+        </button>
+
+        <ul className="wait_list">
+          <li className="wait_item">
+            <span className="wait_span hr_lini">ID</span>
+            <span className="wait_span hr_lini">Фамилия</span>
+            <span className="wait_span hr_lini">Имя</span>
+            <span className="wait_span hr_lini">Изменить</span>
+          </li>
+
+          <li className="wait_item">
+            <p className="wait_suptitle hr_lini">#1</p>
+            <p className="wait_suptitle hr_lini">Тыныбекова</p>
+            <p className="wait_suptitle hr_lini">Айсулуу</p>
+            <div className="wait_edi_delet">
+              <button className="btn_edit hr_lini1" onClick={() => setModalOpen(true)}>
+                <MdEditDocument />
+              </button>
+              <button className="btn_delet hr_lini1" onClick={() => setDeleteModalOpen(true)}>
+                <RiDeleteBinFill />
+              </button>
+            </div>
+          </li>
+        </ul>
+      </section>
+
+      <Waiter_modal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
+
+      {deleteModalOpen && (
+        <div className="delet_overlay" onClick={() => setDeleteModalOpen(false)}>
+          <div className="delet_modal" onClick={(e) => e.stopPropagation()}>
+            <p className="delet_title">
+              Вы точно уверены, что хотите удалить учётную запись?
+            </p>
+            <div className="delet_group">
+              <button className="delet_otmen" onClick={() => setDeleteModalOpen(false)}>
+                Отменить
+              </button>
+              <button className="delet_podver">
+                Подтвердить
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
