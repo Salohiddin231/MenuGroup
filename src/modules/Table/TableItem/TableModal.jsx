795abf05e5fc305d42214744b26c28a0da9ab142
abdumutalibovumar <umarabdumutalibov2010@gmail.com>
2025-06-28 09:43:41 +0600

    table_work

--- src/modules/Table/TableItem/TableModal.jsx
+++ src/modules/Table/TableItem/TableModal.jsx
@@ -23,7 +23,7 @@ export default function TableModal({ isOpen, onClose }) {
         left: 0,
         width: '100vw',
         height: '100vh',
-        background: 'rgba(0,0,0,0.2)',
+        background: 'rgba(0,0,0,0.3)',
         zIndex: 1000,
         display: 'flex',
         alignItems: 'center',
